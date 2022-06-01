954dd38df6bf97607cb020856bbc2255a56e9880
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-06-01 08:26:11 -0500

    edit params

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 
-DOCTOR_NAME = "williams"
+DOCTOR_NAME = input("Enter the doctor name: \n")
+DOCTOR_QUALIFIER = input("Enter city, state, or zip code: \n")
+
 
 DOCTOR_ENDPOINT = "https://clinicaltables.nlm.nih.gov/api/npi_idv/v3/search"
 
@@ -8,7 +10,7 @@ DOCTOR_ENDPOINT = "https://clinicaltables.nlm.nih.gov/api/npi_idv/v3/search"
 doctor_parameters = {
     "terms": DOCTOR_NAME,
     "maxList": 5,
-    "q": "OK"
+    "q": DOCTOR_QUALIFIER
 }
 
 doctor_info = requests.get(DOCTOR_ENDPOINT, params=doctor_parameters)
@@ -16,3 +18,4 @@ doctor_info.raise_for_status()
 doctors = doctor_info.json()
 
 print(doctors)
+print(doctor_info.status_code)
