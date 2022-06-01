73e96683a1afa81753406233d0c5ed9c2c0745b9
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-05-31 21:53:33 -0500

    retrieve json

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ doctor_parameters = {
     "terms": DOCTOR_NAME,
     "maxList": 5,
 }
+
+doctor_info = requests.get(DOCTOR_ENDPOINT, params=doctor_parameters)
+doctor_info.raise_for_status()
+doctors = doctor_info.json()
+
+print(doctors)
