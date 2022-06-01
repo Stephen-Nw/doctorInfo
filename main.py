0370ab88dc9980957817463b111d9276317c36a6
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-05-31 21:48:57 -0500

    add api parameters

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import requests
 
+DOCTOR_NAME = "Vasudevan"
+
 DOCTOR_ENDPOINT = "https://clinicaltables.nlm.nih.gov/api/npi_idv/v3/search"
+
+
+doctor_parameters = {
+    "terms": DOCTOR_NAME,
+    "maxList": 5,
+}
