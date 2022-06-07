a5d3af653dd544c1500615998a421c98601ac77d
Stephen Nwaukwa <interprimos80@hotmail.com>
2022-06-07 07:31:48 -0500

    add comment

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 import requests
 
-DOCTOR_LAST_NAME = input("Enter the doctor's last name: \n").upper()
-DOCTOR_QUALIFIER = input("Enter city, state, or zip code: \n")
-MAX_LIST = 5
 
+def doctor_list():
 
-DOCTOR_ENDPOINT = "https://clinicaltables.nlm.nih.gov/api/npi_idv/v3/search"
+    DOCTOR_LAST_NAME = input("Enter the doctor's last name: \n").upper()
+    DOCTOR_QUALIFIER = input("Enter city, state, or zip code: \n")
+    MAX_LIST = 5
 
+    DOCTOR_ENDPOINT = "https://clinicaltables.nlm.nih.gov/api/npi_idv/v3/search"
 
-doctor_parameters = {
-    "terms": DOCTOR_LAST_NAME,
-    "maxList": MAX_LIST,
-    "q": DOCTOR_QUALIFIER
-}
+    doctor_parameters = {
+        "terms": DOCTOR_LAST_NAME,
+        "maxList": MAX_LIST,
+        "q": DOCTOR_QUALIFIER
+    }
 
-try:
-    raw_data = requests.get(DOCTOR_ENDPOINT, params=doctor_parameters)
-    raw_data.raise_for_status()
-except requests.exceptions.HTTPError as err:
-    print(err)
-else:
+    try:
+        raw_data = requests.get(DOCTOR_ENDPOINT, params=doctor_parameters)
+        raw_data.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(err)  # placeholder - redirect to a 404 page
+    else:
 
-    doctors_data = raw_data.json()
+        doctors_data = raw_data.json()
 
-    original_list_of_doctors = doctors_data[3]
+        original_list_of_doctors = doctors_data[3]
 
-    clean_doctor_list = [
-        doctor for doctor in original_list_of_doctors if DOCTOR_LAST_NAME in doctor[0]]
+        clean_doctor_list = [
+            doctor for doctor in original_list_of_doctors if DOCTOR_LAST_NAME in doctor[0]]
 
-    print(f"Returning the first {len(clean_doctor_list)} doctors")
-    print(clean_doctor_list)
+        print(f"Returning the first {len(clean_doctor_list)} doctors")
+        print(clean_doctor_list)
